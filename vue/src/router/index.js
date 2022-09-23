ab5da9a511c03671af5a874b240684461beb9f93
amr <finerteam@gmail.com>
2022-09-24 01:25:51 +0200

    create default layout

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -2,22 +2,30 @@ import {createRouter, createWebHistory} from "vue-router";
 import Dashboard from "../views/Dashboard.vue";
 import Login from "../views/Login.vue";
 import Register from "../views/Register.vue";
+import DefaultLayout from '../components/DefaultLayout.vue'
 
 const routes = [
     {
-        path:'/',
-        name:'Dashboard',
-        component:Dashboard
+        path: '/',
+        redirect: '/dashboard',
+        component: DefaultLayout,
+        children: [
+            {
+                path: '/dashboard',
+                name: 'Dashboard',
+                component: Dashboard
+            }
+        ]
     },
     {
-        path:'/login',
-        name:'Login',
-        component:Login
+        path: '/login',
+        name: 'Login',
+        component: Login
     },
     {
-        path:'/register',
-        name:'Register',
-        component:Register
+        path: '/register',
+        name: 'Register',
+        component: Register
     }
 ]
 const router = createRouter({
---
+++ vue/src/views/survey.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+    name: "survey"
+}
+</script>
+
+<style scoped>
+
+</style>
