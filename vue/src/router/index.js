868e3b33e6d91a89cc593d20db453f907c2876bb
amr <finerteam@gmail.com>
2022-09-24 02:00:42 +0200

    create auth layout

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -3,33 +3,49 @@ import Dashboard from "../views/Dashboard.vue";
 import Login from "../views/Login.vue";
 import Register from "../views/Register.vue";
 import DefaultLayout from '../components/DefaultLayout.vue'
+import Surveys from "../views/Surveys.vue";
+import store from "../store/index.js";
+import AuthLayout from "../components/AuthLayout.vue";
 
 const routes = [
     {
         path: '/',
         redirect: '/dashboard',
         component: DefaultLayout,
+        meta: {requiresAuth: true},
         children: [
-            {
-                path: '/dashboard',
-                name: 'Dashboard',
-                component: Dashboard
-            }
+            {path: '/dashboard', name: 'Dashboard', component: Dashboard},
+            {path: '/surveys', name: 'Surveys', component: Surveys},
         ]
     },
     {
-        path: '/login',
-        name: 'Login',
-        component: Login
+        path: '/auth',
+        name: 'Auth',
+        redirect: '/login',
+        meta: {isGuest: true},
+        component: AuthLayout,
+        children: [
+            {path: '/login', name: 'Login', component: Login},
+            {path: '/register', name: 'Register', component: Register}
+        ]
     },
-    {
-        path: '/register',
-        name: 'Register',
-        component: Register
-    }
+
 ]
 const router = createRouter({
     history: createWebHistory(),
     routes
+})
+
+// check if the user is login
+router.beforeEach((to, from, next) => {
+    if (to.meta.requiresAuth && !store.state.user.token) {
+        next({name: 'Login'})
+    } else if (store.state.user.token && to.meta.isGuest) {
+        // prevent user to access login page or Register page if he is authorized
+        next({name: 'Dashboard'})
+    } else {
+        next();
+    }
+
 })
 export default router;
---
+++ vue/src/views/Surveys.vue
@@ -0,0 +1,19 @@
+<template>
+    <header className="bg-white shadow">
+        <div className="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
+            <h1 className="text-3xl font-bold tracking-tight text-gray-900">Surveys</h1>
+        </div>
+    </header>
+    <main>
+        <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+            <!-- Replace with your content -->
+            <div className="px-4 py-6 sm:px-0">
+                Content goes here
+            </div>
+            <!-- /End replace -->
+        </div>
+    </main>
+</template>
+<script>
+export default {}
+</script>
